ab74e40fbd9fc77fd989ede50273d2da1c2ab4ca
GuruBalajim <162150376+GuruBalajim@users.noreply.github.com>
2024-05-03 13:12:45 +0530

    Add files via upload

---
+++ 02.05.2024/RatingUI/interactive-rating-component-main/index.js
@@ -0,0 +1,6 @@
+// var rating =document.getElementById("rate").value
+// var done=document.getElementById("Submit")
+const display =document.getElementById("ra")
+function rating(input){
+display.value =input;
+}
